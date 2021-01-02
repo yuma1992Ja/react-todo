06a316d1cfb938500f26212dae0a3fbe12b46558
yuma1992Ja <39455191+yuma1992Ja@users.noreply.github.com>
2021-01-02 22:04:15 +0900

    構造作成 作成イメージを作成する

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,43 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div>OK</div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>あああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>いいいいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>あああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>いいいいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div></div>
+    </>
+  );
 };
